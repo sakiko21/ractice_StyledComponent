20a70e11321fa06da65c8c3335f7ff42d7640275
sakiko21 <92835229+sakiko21@users.noreply.github.com>
2023-02-14 00:39:55 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
+import "./styles.css";
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+    </div>
+  );
+}
